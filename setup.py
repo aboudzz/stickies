722a0b229cf2f256a86ee0ab532eaa0f93cdcc22
Aboud Zakaria <aboud.zakaria@gmail.com>
2017-11-14 16:28:01 +0100

    force icon path in .desktop file

--- setup.py
+++ setup.py
@@ -25,17 +25,18 @@
 import glob, os 
 from distutils.core import setup
 
-inst_path = '/usr/share/com.github.aboudzakaria.stickies/stickies'
+inst_path = '/usr/share/com.github.aboudzakaria.stickies'
 
 install_data = [('/usr/share/metainfo', ['data/com.github.aboudzakaria.stickies.appdata.xml']),
                 ('/usr/share/applications', ['data/com.github.aboudzakaria.stickies.desktop']),
                 ('/usr/share/icons/hicolor/128x128/apps',['data/com.github.aboudzakaria.stickies.png']),
-                (inst_path+'/StickyManager',['stickies/StickyManager/StickyDTO.py']),
-                (inst_path+'/StickyManager',['stickies/StickyManager/StickyUtils.py']),
-                (inst_path+'/StickyManager',['stickies/StickyManager/StickyWindow.py']),
-                (inst_path+'/StickyManager',['stickies/StickyManager/__init__.py']),
-                (inst_path,['stickies/main.py']),
-                (inst_path,['stickies/__init__.py']),]
+                (inst_path+'/stickies/StickyManager',['stickies/StickyManager/StickyDTO.py']),
+                (inst_path+'/stickies/StickyManager',['stickies/StickyManager/StickyUtils.py']),
+                (inst_path+'/stickies/StickyManager',['stickies/StickyManager/StickyWindow.py']),
+                (inst_path+'/stickies/StickyManager',['stickies/StickyManager/__init__.py']),
+                (inst_path+'/stickies',['stickies/main.py']),
+                (inst_path+'/stickies',['stickies/__init__.py']),
+                (inst_path, ['data/com.github.aboudzakaria.stickies.png']),]
 
 setup(  name='stickies',
         version='0.1.6',
